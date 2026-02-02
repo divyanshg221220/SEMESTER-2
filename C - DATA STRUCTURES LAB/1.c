befb2e3a947bffe34fac392c43e22e9eeaa6aaad
DIVYANSH GUPTA <divyanshg221220@outlook.com>
2026-02-02 10:59:51 +0000

    fix: corrected function return type

--- C - DATA STRUCTURES LAB/1.c
+++ C - DATA STRUCTURES LAB/1.c
@@ -1,6 +1,6 @@
 // Write a program to calculate the length of string using pointer.
 #include <stdio.h>
-int string_length(char *str)
+void string_length(char *str)
 {
     int length = 0;
     while (*str != '\0')
@@ -10,10 +10,11 @@ int string_length(char *str)
     }
     printf("length: %d\n", length);
 }
-int main()
+int main(int argc, char const *argv[])
 {
     char str[100];
     printf("Enter a string: ");
     scanf("%[^\n]s", str);
     string_length(str);
+    return 0;
 }
