0da706949c5794364c5f357739f50fc3d93f465d
divyanshg221220 <divyanshg221220@outlook.com>
2026-02-10 23:22:57 +0530

    fix: fixed the insertion deletion bug

--- C/insertion and deletion at beginning index and end.c
+++ C/insertion and deletion at beginning index and end.c
@@ -5,9 +5,18 @@ int main(int argc, char const *argv[])
     printf("Enter number of elements: ");
     scanf("%d", &n);
     int A[n];
+    for (int i = 0; i < n; i++)
+    {
+        A[i] = 0;
+    }
     int q;
     do
     {
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d ", A[i]);
+        }
+        printf("\n");
         printf("1. Insertion\n");
         printf("2. Deletion\n");
         printf("3. EXIT\n");
@@ -16,89 +25,88 @@ int main(int argc, char const *argv[])
         switch (q)
         {
         case 1:
-            int val, q1;
-            printf("Enter element to insert: ");
-            scanf("%d", &val);
-            printf("1. Insert at beginning\n");
-            printf("2. Insert at index\n");
-            printf("3. Insert at end\n");
-            printf("Enter your choice : ");
-            scanf("%d", &q1);
-            switch (q1)
             {
-            case 1:
-                for (int i = n; i > 0; i--)
+                int val, q1;
+                printf("Enter element to insert: ");
+                scanf("%d", &val);
+                printf("1. Insert at beginning\n");
+                printf("2. Insert at index\n");
+                printf("3. Insert at end\n");
+                printf("Enter your choice : ");
+                scanf("%d", &q1);
+                switch (q1)
                 {
-                    A[i] = A[i - 1];
+                case 1:
+                    for (int i = n -1; i > 0; i--)
+                    {
+                        A[i] = A[i - 1];
+                    }
+                    A[0] = val;
+                    break;
+                case 2:
+                    {
+                        int index;
+                        printf("Enter index: ");
+                        scanf("%d", &index);
+                        for (int i = n - 1; i > index; i--)
+                        {
+                            A[i] = A[i - 1];
+                        }
+                        A[index] = val;
+                        break;
+                    };
+                case 3:
+                    A[n - 1] = val;
+                    break;
+                default:
+                    printf("Invalid Input\n");
+                    break;
                 }
-                A[0] = val;
-                n++;
                 break;
-            case 2:
-                int index;
-                printf("Enter index: ");
-                scanf("%d", &index);
-                for (int i = n; i > index; i--)
-                {
-                    A[i] = A[i + 1];
-                }
-                A[index] = val;
-                n++;
-                break;
-            case 3:
-                A[n] = val;
-                n++;
-                break;
-            default:
-                printf("Invalid Input\n");
-                break;
-            }
-            break;
+            };
         case 2:
-            int q2;
-            printf("1. Delete at beginning\n");
-            printf("2. Delete at index\n");
-            printf("3. Delete at end\n");
-            printf("Enter your choice : ");
-            scanf("%d", &q2);
-            switch (q2)
             {
-            case 1:
-                for (int i = 0; i < n - 1; i++)
+                int q2;
+                printf("1. Delete at beginning\n");
+                printf("2. Delete at index\n");
+                printf("3. Delete at end\n");
+                printf("Enter your choice : ");
+                scanf("%d", &q2);
+                switch (q2)
                 {
-                    A[i] = A[i + 1];
+                case 1:
+                    for (int i = 0; i < n - 1; i++)
+                    {
+                        A[i] = A[i + 1];
+                    }
+                    A[n - 1] = 0;
+                    break;
+                case 2:
+                    {
+                        int index;
+                        printf("Enter index: ");
+                        scanf("%d", &index);
+                        for (int i = index; i < n - 1; i++)
+                        {
+                            A[i] = A[i + 1];
+                        }
+                        break;
+                    };
+                case 3:
+                    A[n - 1] = 0;
+                    break;
+                default:
+                    printf("Invalid Input\n");
+                    break;
                 }
-                n--;
                 break;
-            case 2:
-                int index;
-                printf("Enter index: ");
-                scanf("%d", &index);
-                for (int i = index; i < n - 1; i++)
-                {
-                    A[i] = A[i + 1];
-                }
-                n--;
-                break;
-            case 3:
-                n--;
-                break;
-            default:
-                printf("Invalid Input\n");
-                break;
-            }
-            break;
+            };
         case 3:
             break;
         default:
             printf("Invalid Input\n");
             break;
-        }   
-        for (int i = 0; i < n; i++)
-        {
-            printf("%d ", A[i]);
         }
-        printf("\n");
     } while (q != 3);
     return 0;
 }
