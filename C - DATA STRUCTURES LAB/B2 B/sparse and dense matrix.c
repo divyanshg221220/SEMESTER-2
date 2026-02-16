16b8d0088d13a97978e23d2dc153bd8e296c3a30
divyanshg221220 <divyanshg221220@outlook.com>
2026-02-17 00:05:09 +0530

    fix: using 2d array instead of row column and value

---
+++ C - DATA STRUCTURES LAB/B2 B/sparse and dense matrix.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int x, y;
+    printf("Enter number of rows: ");
+    scanf("%d", &x);
+    printf("Enter number of columns: ");
+    scanf("%d", &y);
+    int matrix[x][y];
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            printf("Enter (%d, %d) element: ", i+1, j+1);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    int count_zero = 0;
+    printf("matrix: \n");
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            printf("%d ", matrix[i][j]);
+            if (matrix[i][j] == 0)
+            {
+                count_zero++;
+            }
+        }
+        printf("\n");
+    }
+    if (count_zero > (x*y)/2)
+    {
+        printf("sparse matrix\n");
+    }
+    else
+    {
+        printf("dense matrix\n");
+    }
+    int count_non_zero = (x * y) - count_zero;
+    int matrix_representation[count_non_zero][3];
+    int k = 0;
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            if (matrix[i][j] != 0)
+            {
+                matrix_representation[k][0] = i;
+                matrix_representation[k][1] = j;
+                matrix_representation[k][2] = matrix[i][j];
+                k++;
+            }
+        }
+    }
+    printf("matrix representation:\n");
+    printf("Row\tColumn\tValue\n");
+    for (int i = 0; i < count_non_zero; i++)
+    {
+        printf("%d\t%d\t%d\n", matrix_representation[i][0], matrix_representation[i][1], matrix_representation[i][2]);
+    }
+    return 0;
+}
