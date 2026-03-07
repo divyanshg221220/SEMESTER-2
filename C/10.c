919fd61b3b4ebeed0149e97f4ca0e1dc3a4b73e6
divyanshg221220 <divyanshg221220@outlook.com>
2026-03-07 22:29:44 +0530

    merge sort

---
+++ C/10.c
@@ -0,0 +1,72 @@
+// Write a program to perform Merge Sort
+#include <stdio.h>
+void merge(int arr[], int low, int mid, int high)
+{
+    int n1 = mid - low + 1;
+    int n2 = high - mid;
+    int left[n1], right[n2];
+    for (int i = 0; i < n1; i++)
+    {
+        left[i] = arr[low + i];
+    }
+    for (int i = 0; i < n2; i++)
+    {
+        right[i] = arr[mid + 1 + i];
+    }
+    int i = 0, j = 0, k = low;
+    while (i < n1 && j < n2)
+    {
+        if (left[i] <= right[j])
+        {
+            arr[k] = left[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1)
+    {
+        arr[k] = left[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        arr[k] = right[j];
+        j++;
+        k++;
+    }
+}
+void mergeSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int mid = low + (high - low) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+}
+int main(int argc, char const *argv[])
+{
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    mergeSort(arr, 0, n - 1);
+    printf("sorted array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
