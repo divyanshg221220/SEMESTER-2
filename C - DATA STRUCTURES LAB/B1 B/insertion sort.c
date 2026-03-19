1187b469a0cc3e16b804dcf4ea8c15d4c1e20532
DIVYANSH GUPTA <divyanshg221220@outlook.com>
2026-03-19 04:50:09 +0000

    sorting

---
+++ C - DATA STRUCTURES LAB/B1 B/insertion sort.c
@@ -0,0 +1,31 @@
+// Write a program to perform Insertion Sort
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+    printf("sorted array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
