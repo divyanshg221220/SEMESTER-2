3e51ed4f9a302ee6cc892d1a90178010ff17d5f0
divyanshg221220 <divyanshg221220@outlook.com>
2026-05-07 14:15:23 +0530

    // Write a program to reverse a string using Stack

---
+++ C - DATA STRUCTURES LAB/16.c
@@ -0,0 +1,25 @@
+// Write a program to reverse a string using Stack
+#include <stdio.h>
+#include <string.h>
+#define MAX 50
+char stack[MAX];
+int top =-1;
+void push(char ch) {
+    stack[++top] = ch;
+}
+char pop() {
+    return stack[top--];
+}
+int main() {
+    char str[MAX];
+    int i, len;
+    printf("Enter a string: ");
+    gets(str);
+    len = strlen(str);
+    for(i = 0; i < len; i++)
+        push(str[i]);
+    for(i = 0; i < len; i++)
+        str[i] = pop();
+    printf("Reversed string: %s\n", str);
+    return 0;
+}
