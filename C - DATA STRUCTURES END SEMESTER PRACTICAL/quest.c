f4b8cb996d410ad073e889ba7d04f3a3630d93dd
divyanshg221220 <divyanshg221220@outlook.com>
2026-05-12 23:55:58 +0530

    .

--- C - DATA STRUCTURES END SEMESTER PRACTICAL/quest.c
+++ C - DATA STRUCTURES END SEMESTER PRACTICAL/quest.c
@@ -1,5 +1,5 @@
 // A. WAP for polynomial representation and arithmetic
-// B. WAP to check whether a matrix is a sparse or not, Also WAP to check balanced paranthesis.
+// B. WAP to check whether a matrix is a Sparse or not, Also WAP to check balanced paranthesis.
 // C. WAP to reverse a string using stack.
 // D. WAP to implement Queue using array perform
 // ->insertion
