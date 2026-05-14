00f3fe83b690af4a98766a0b10c5895368018d61
divyanshg221220 <divyanshg221220@outlook.com>
2026-05-15 00:44:02 +0530

    end sem practical questions

---
+++ C - DATA STRUCTURES END SEMESTER PRACTICAL/C.c
@@ -0,0 +1,25 @@
+// C. WAP to reverse a string using stack.
+#include <stdio.h>
+#include <string.h>
+#define MAX 50
+char stack[MAX];
+int top =-1;
+void push(char ch) {
+    stack[++top] = ch;
+}
+char pop() {
+    return stack[top--];
+}
+int main() {
+    char str[MAX];
+    int i, len;
+    printf("Enter a string: ");
+    gets(str);
+    len = strlen(str);
+    for(i = 0; i < len; i++)
+        push(str[i]);
+    for(i = 0; i < len; i++)
+        str[i] = pop();
+    printf("Reversed string: %s\n", str);
+    return 0;
+}
--- C - DATA STRUCTURES END SEMESTER PRACTICAL/quest.c
+++
@@ -1,6 +0,0 @@
-// A. WAP for polynomial representation and arithmetic
-// B. WAP to check whether a matrix is a Sparse or not, Also WAP to check balanced paranthesis.
-// C. WAP to reverse a string using stack.
-// D. WAP to implement Queue using array perform
-// ->insertion
-// ->Deletion
