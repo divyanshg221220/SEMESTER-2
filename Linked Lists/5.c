75609a0d835e6b01f2ef804d68eaec10750601b7
divyanshg221220 <divyanshg221220@outlook.com>
2026-06-14 20:14:13 +0530

    // Write a program that prints whether the given linked list is sorted (in ascending order) or not.

--- Linked Lists/5.c
+++ Linked Lists/5.c
@@ -1,1 +1,66 @@
 // Write a program that prints whether the given linked list is sorted (in ascending order) or not.
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *head = NULL;
+void insert(int val)
+{
+    struct node *new = malloc(sizeof(struct node));
+    new -> data = val;
+    new -> next = NULL;
+    if (head == NULL)
+    {
+        head = new;
+        return;
+    }
+    struct node *temp = head;
+    while (temp -> next != NULL)
+    {
+        temp = temp -> next;
+    }
+    temp -> next = new;
+}
+void sort()
+{
+    struct node *temp = head;
+    while (temp != NULL && temp -> next != NULL)
+    {
+        if (temp -> data > temp -> next -> data)
+        {
+            printf("not sorted");
+            return;
+        }
+        temp = temp -> next;
+    }
+    printf("sorted");
+}
+void display()
+{
+    struct node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp -> data);
+        temp = temp -> next;
+    }
+    printf("NULL\n");
+}
+int main(int argc, char const *argv[])
+{
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int val;
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the element %d: ", i + 1);
+        scanf("%d", &val);
+        insert(val);
+    }
+    display();
+    sort();
+    return 0;
+}
