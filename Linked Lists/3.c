860d48344be6e2854dfc3fc1188552524a30b51c
divyanshg221220 <divyanshg221220@outlook.com>
2026-06-14 20:01:52 +0530

    // Write a program to multiply every element of the linked list with 10.

--- Linked Lists/3.c
+++ Linked Lists/3.c
@@ -1,1 +1,62 @@
 // Write a program to multiply every element of the linked list with 10.
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *head = NULL;
+void insert(int val)
+{
+    struct node *new = malloc(sizeof(struct node));
+    new -> data = val;
+    new -> next = NULL;
+    if (head == NULL)
+    {
+        head = new;
+        return;
+    }
+    struct node *temp = head;
+    while (temp -> next != NULL)
+    {
+        temp = temp -> next;
+    }
+    temp -> next = new;
+}
+void multiply()
+{
+    struct node *temp = head;
+    while (temp != NULL)
+    {
+        temp -> data *= 10;
+        temp = temp -> next;
+    }
+}
+void display()
+{
+    struct node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp -> data);
+        temp = temp -> next;
+    }
+    printf("NULL\n");
+}
+int main(int argc, char const *argv[])
+{
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int val;
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the element %d: ", i + 1);
+        scanf("%d", &val);
+        insert(val);
+    }
+    display();
+    multiply();
+    display();
+    return 0;
+}
