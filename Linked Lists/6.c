4e0de7673a9a691aacbe7e95d9abd4173dbb10ae
divyanshg221220 <divyanshg221220@outlook.com>
2026-06-14 21:12:11 +0530

    // Write a program that copies a circular linked list.

---
+++ Linked Lists/6.c
@@ -0,0 +1,73 @@
+// Write a program that copies a circular linked list.
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+    int data;
+    struct node *next;
+};
+struct node *head1 = NULL;
+struct node *head2 = NULL;
+void insert(struct node **head, int val)
+{
+    struct node *new = malloc(sizeof(struct node));
+    new -> data = val;
+    new -> next = NULL;
+    if (*head == NULL)
+    {
+        *head = new;
+        new -> next = *head;
+        return;
+    }
+    struct node *temp = *head;
+    while (temp -> next != *head)
+    {
+        temp = temp -> next;
+    }
+    temp -> next = new;
+    new -> next = *head;
+}
+void copy()
+{
+    if (head1 == NULL)
+    {
+        return;
+    }
+    struct node *temp1 = head1;
+    do
+    {
+        insert(&head2, temp1 -> data);
+        temp1 = temp1 -> next;
+    } while (temp1 != head1);
+}
+void display(struct node *head)
+{
+    if (head == NULL)
+    {
+        printf("NULL\n");
+        return;
+    }
+    struct node *temp = head;
+    do
+    {
+        printf("%d -> ", temp -> data);
+        temp = temp -> next;
+    } while (temp != head);
+    printf("HEAD\n");
+}
+int main(int argc, char const *argv[])
+{
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int val;
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the element %d: ", i + 1);
+        scanf("%d", &val);
+        insert(&head1, val);
+    }
+    display(head1);
+    copy();
+    display(head2);
+    return 0;
+}
