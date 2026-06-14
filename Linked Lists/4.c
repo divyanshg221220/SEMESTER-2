b9867638e90e37a3a68b91aa0d3dd44b1a16f75d
divyanshg221220 <divyanshg221220@outlook.com>
2026-06-14 20:05:27 +0530

    // Write a program to print the number of non-zero elements in the list.

--- Linked Lists/4.c
+++ Linked Lists/4.c
@@ -1,1 +1,66 @@
 // Write a program to print the number of non-zero elements in the list.
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *head = NULL;
+void insert(int val)
+{
+    struct node *new = malloc(sizeof(struct node));
+    new -> data = val;
+    new -> next = NULL;
+    if (head == NULL)
+    {
+        head = new;
+        return;
+    }
+    struct node *temp = head;
+    while (temp -> next != NULL)
+    {
+        temp = temp -> next;
+    }
+    temp -> next = new;
+}
+void count()
+{
+    struct node *temp = head;
+    int count = 0;
+    while (temp != NULL)
+    {
+        if (temp -> data != 0)
+        {
+            count++;
+        }
+        temp = temp -> next;
+    }
+    printf("number of non-zero elements: %d", count);
+}
+void display()
+{
+    struct node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp -> data);
+        temp = temp -> next;
+    }
+    printf("NULL\n");
+}
+int main(int argc, char const *argv[])
+{
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int val;
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the element %d: ", i + 1);
+        scanf("%d", &val);
+        insert(val);
+    }
+    display();
+    count();
+    return 0;
+}
