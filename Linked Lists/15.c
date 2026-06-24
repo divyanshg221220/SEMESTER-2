d5dceccbd3748cef85301b776f41f94d83dc5f99
divyanshg221220 <divyanshg221220@outlook.com>
2026-06-24 14:14:04 +0530

    // Write a program to reverse a linked list using recursion.

---
+++ Linked Lists/15.c
@@ -0,0 +1,71 @@
+// Write a program to reverse a linked list using recursion.
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *head = NULL;
+void insert(int val)
+{
+    struct node *new = malloc(sizeof(struct node));
+    new -> data = val;
+    new -> next = NULL;
+    if (head == NULL)
+    {
+        head = new;
+        return;
+    }
+    struct node *temp = head;
+    while (temp -> next != NULL)
+    {
+        temp = temp -> next;
+    }
+    temp -> next = new;
+}
+void reverse()
+{
+    if (head == NULL || head -> next == NULL)
+    {
+        return;
+    }
+    struct node *curr = head;
+    head = head -> next;
+    reverse();
+    struct node *temp = head;
+    while (temp -> next != NULL)
+    {
+        temp = temp -> next;
+    }
+    temp -> next = curr;
+    curr -> next = NULL;
+}
+void display()
+{
+    struct node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp -> data);
+        temp = temp -> next;
+    }
+    printf("NULL\n");
+}
+int main(int argc, char const *argv[])
+{
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int val;
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &val);
+        insert(val);
+    }
+    display();
+    reverse();
+    printf("After reversing\n");
+    display();
+    return 0;
+}
