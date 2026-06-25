0b58a2e287d53d2b6c099d2495a5cecf5c68e61b
divyanshg221220 <divyanshg221220@outlook.com>
2026-06-26 01:08:40 +0530

    // Write a program to count the number of occurrences of a given value in a linked list.

---
+++ Linked Lists/21.c
@@ -0,0 +1,68 @@
+// Write a program to count the number of occurrences of a given value in a linked list.
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *head = NULL;
+void insert(int val)
+{
+    struct node *new = malloc(sizeof(struct node));
+    new -> data = val;
+    new -> next = NULL;
+    if (head == NULL)
+    {
+        head = new;
+        return;
+    }
+    struct node *temp = head;
+    while (temp -> next != NULL)
+    {
+        temp = temp -> next;
+    }
+    temp -> next = new;
+}
+void count(int val)
+{
+    struct node *temp = head;
+    int count = 0;
+    while (temp != NULL)
+    {
+        if (temp -> data == val)
+        {
+            count++;
+        }
+        temp = temp -> next;
+    }
+    printf("Number of occurrences of %d: %d", val, count);
+}
+void display()
+{
+    struct node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp -> data);
+        temp = temp -> next;
+    }
+    printf("NULL\n");
+}
+int main(int argc, char const *argv[])
+{
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int val;
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the element %d: ", i + 1);
+        scanf("%d", &val);
+        insert(val);
+    }
+    display();
+    printf("Enter the element to be searched: ");
+    scanf("%d", &val);
+    count(val);
+    return 0;
+}
