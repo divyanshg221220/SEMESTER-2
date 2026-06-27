7959bd9d18d8ec1c595ae7d74344a25d6f4ac468
divyanshg221220 <divyanshg221220@outlook.com>
2026-06-27 18:34:12 +0530

    // Write a program to create a singly linked list and reverse the list by interchanging the links and not the data.

---
+++ Linked Lists/33.c
@@ -0,0 +1,68 @@
+// Write a program to create a singly linked list and reverse the list by interchanging the links and not the data.
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *head = NULL;
+void insert(int val)
+{
+    struct node *new = malloc(sizeof(struct node));
+    new -> data = val;
+    new -> next = NULL;
+    if (head == NULL)
+    {
+        head = new;
+        return;
+    }
+    struct node *temp = head;
+    while (temp -> next != NULL)
+    {
+        temp = temp -> next;
+    }
+    temp -> next = new;
+}
+void reverse()
+{
+    struct node *prev = NULL;
+    struct node *temp = head;
+    struct node *next = NULL;
+    while (temp != NULL)
+    {
+        next = temp -> next;
+        temp -> next = prev;
+        prev = temp;
+        temp = next;
+    }
+    head = prev;
+}
+void display()
+{
+    struct node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp -> data);
+        temp = temp -> next;
+    }
+    printf("NULL\n");
+}
+int main(int argc, char const *argv[])
+{
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int val;
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the element %d: ", i + 1);
+        scanf("%d", &val);
+        insert(val);
+    }
+    display();
+    reverse();
+    printf("After reversing\n");
+    display();
+    return 0;
+}
