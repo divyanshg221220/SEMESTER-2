aa459cc1ff1c43add12662bb550547f189731941
divyanshg221220 <divyanshg221220@outlook.com>
2026-06-28 02:06:45 +0530

    // Write a program to create a header linked list.

---
+++ Linked Lists/37.c
@@ -0,0 +1,54 @@
+// Write a program to create a header linked list.
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *header = NULL;
+__attribute__((constructor))
+void init()
+{
+    header = malloc(sizeof(struct node));
+    header -> data = -1;
+    header -> next = NULL;
+}
+void insert(int val)
+{
+    struct node *new = malloc(sizeof(struct node));
+    new -> data = val;
+    new -> next = NULL;
+    struct node *temp = header;
+    while (temp -> next != NULL)
+    {
+        temp = temp -> next;
+    }
+    temp -> next = new;
+}
+void display()
+{
+    printf("HEADER -> ");
+    struct node *temp = header -> next;
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp -> data);
+        temp = temp -> next;
+    }
+    printf("NULL\n");
+}
+int main(int argc, char const *argv[])
+{
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int val;
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the element %d: ", i + 1);
+        scanf("%d", &val);
+        insert(val);
+    }
+    display();
+    return 0;
+}
