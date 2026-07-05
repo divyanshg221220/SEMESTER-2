db8844f54ccf6af63738f85715353962663f9efc
divyanshg221220 <divyanshg221220@outlook.com>
2026-07-06 01:52:52 +0530

    minor fix

--- Stacks/1.c
+++ Stacks/1.c
@@ -77,6 +77,7 @@ int main(int argc, char const *argv[])
             break;
         default:
             printf("Invalid choice\n");
+            break;
         }
     } while (choice != 5);
     return 0;
