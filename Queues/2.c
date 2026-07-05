3ffb661e36a3a8bb598ee900fe3d95684b0b1ba2
divyanshg221220 <divyanshg221220@outlook.com>
2026-07-06 00:01:49 +0530

    // Write a program to create a linear queue of 10 values.

---
+++ Queues/2.c
@@ -0,0 +1,45 @@
+// Write a program to create a linear queue of 10 values.
+#include <stdio.h>
+#include <stdlib.h>
+struct queue
+{
+    int data;
+    struct queue *next;
+};
+struct queue *front = NULL;
+struct queue *rear = NULL;
+void enqueue(int val)
+{
+    struct queue *new = malloc(sizeof(struct queue));
+    new -> data = val;
+    new -> next = NULL;
+    if (rear == NULL)
+    {
+        front = rear = new;
+        return;
+    }
+    rear -> next = new;
+    rear = new;
+}
+void display()
+{
+    struct queue *temp = front;
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp -> data);
+        temp = temp -> next;
+    }
+    printf("NULL\n");
+}
+int main(int argc, char const *argv[])
+{
+    for (int i = 0; i < 10; i++)
+    {
+        int val;
+        printf("Enter the element %d: ", i + 1);
+        scanf("%d", &val);
+        enqueue(val);
+    }
+    display();
+    return 0;
+}
